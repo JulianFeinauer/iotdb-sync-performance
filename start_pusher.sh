83a961d1a445fb9ee07a61db6fd03d90a4cc0694
julian <j.feinauer@pragmaticminds.de>
2022-05-12 18:18:33 +0200

    Final commit

--- start_pusher.sh
+++ start_pusher.sh
@@ -1,2 +1,10 @@
  python main.py 7001 1 1 &\
- python main.py 7002 1 2
+ python main.py 7002 1 2 &\
+ python main.py 7003 1 3 &\
+ python main.py 7004 1 4 &\
+ python main.py 7005 1 5 &\
+ python main.py 7006 1 6 &\
+ python main.py 7007 1 7 &\
+ python main.py 7008 1 8 &\
+ python main.py 7009 1 9 &\
+ python main.py 7010 1 10
