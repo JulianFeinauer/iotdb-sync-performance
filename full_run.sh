34ceceda8649e9144a411a5edf14a853c2ccacc9
julian <j.feinauer@pragmaticminds.de>
2022-05-13 13:08:28 +0200

    Cleanup sync servers

--- full_run.sh
+++ full_run.sh
@@ -2,5 +2,7 @@ rm test.pcap
 screen -d -S dump -m tcpdump -i lo -w test.pcap dst port 5555 or portrange 7001-7100
 python3 setup_iotdb.py
 screen -S dump -X quit
+# Cleanup Sync Servers
+ps aux | grep SyncClient | awk '{print $2}' | xargs kill
 # Print tshark output
-tshark -Q -r test.pcap -z endpoints,tcp
+# tshark -Q -r test.pcap -z endpoints,tcp
