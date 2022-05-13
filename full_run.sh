ed80fe25d9afccb7a4ef7bc7da0fe6b3a177cbcc
julian <j.feinauer@pragmaticminds.de>
2022-05-13 08:44:01 +0200

    minor additions

--- full_run.sh
+++ full_run.sh
@@ -1,3 +1,5 @@
 screen -d -S dump -m tcpdump -i lo -w test.pcap dst port 5555 or portrange 7001-7100
 python3 setup_iotdb.py
 screen -S dump -X quit
+# Print tshark output
+tshark -Q -r test.pcap -z endpoints,tcp
